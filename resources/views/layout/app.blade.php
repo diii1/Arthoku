254b5c5865f9b485ae82a53fe06e417b371628a7
Raviy Bayu S <bayu.setiaji709@gmail.com>
2021-12-15 14:46:40 +0700

    login-modal

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -38,15 +38,45 @@
                             </li>
                         </ul>
                         <div class="d-flex">
-                            <div class="signIn btn btn-primary me-3">Sign in</div>
-                            <div class="signUp btn btn-primary">Sign Up</div>
+                            <div class="signIn btn btn-primary me-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Sign in</div>
+                            <div class="signUp btn btn-primary"><a href="/register">Sign Up</a></div>
+                        </div>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content modal-card">
+                                    <div class="modal-header d-flex justify-content-center">
+                                        <h5 class="modal-title text-white" id="exampleModalLabel">Sign In</h5>
+                                        <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+                                    </div>
+                                    <div class="modal-body text-white">
+                                        <form>
+                                            <div class="mb-3">
+                                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                                <input type="email" class="form-control" id="exampleInputEmail1">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                                <input type="password" class="form-control" id="exampleInputPassword1">
+                                            </div>
+                                            <div class="mb-3 form-check">
+                                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                            </div>
+                                            <button type="submit" class="btn btn-primary btn-signIn">Sign In</button>
+                                            <a href="/register" class="btn btn-secondary ms-3">Register</a>
+                                            <button type="button" class="btn btn-danger btnClose" data-bs-dismiss="modal">Close</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </nav>
 
             <div class="row">
-
+                <img src="{{ asset('assets/image/icon-feature-1.png') }}" alt="test">
             </div>
         </div>
     </div>
