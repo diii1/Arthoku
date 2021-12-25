96e996d2e91619cee12c1976ab4e9228a321b820
alfyando2001@gmail.com <alfyando2001@gmail.com>
2021-12-25 23:44:38 +0700

    Font

--- resources/views/layout/clientDashboard.blade.php
+++ resources/views/layout/clientDashboard.blade.php
@@ -9,11 +9,14 @@
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/responsiveDashboard.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 </head>
 <body>
-    <div class="body-app">
+    <div class="body">
         <div class="container mt-5 bg-container">
             <div class="row">
                 @include('layout.client.navDashboard')
